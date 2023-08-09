ce7f258f0dcf283d4e75f921f13a2d020118acfe
James Terry <jdt@jamesdterry.com>
2023-08-09 19:57:01 -0400

    Finish defense

--- defense.py
+++ defense.py
@@ -24,6 +24,25 @@ defData = [
     ["'D' OUTS", "10,,,,,", ",11,,,,", ",,12,,,", ",,,13,,", ",,,,14,", ",11,12,,,", ",11,,13,,", ",,12,13,,", ",,12,,14,", ",,,13,14,", ",11,12,13,,", ",11,12,,14,", ",11,,13,14,", ",,12,13,14,", ",,12,,14,15", ",,,13,14,15", "10,,12,,14,15", ",11,12,,14,15", ",11,,13,14,15", ",,12,13,14,15", "10,11,,13,14,15", "10,,12,13,14,15", ",11,12,13,14,15", "10,11,12,13,14,15"]
 ]
 
+sacBuntData = [
+    #     10    11    12    13    14    15    16    17    18    19    20    21    22    23    24    25    26    27    28     29   30    31    32    33   34    35    36    37    38    39    
+    ["A", "SH", "SH", "SH", "SH", "1",  "SH", "SH", "SH", "SH", "SH", "SH", "SH", "SH", "FC", "SH", "SH", "SH", "SH", "DP", "SH", "2K", "2K", "2K", "E", "SH", "G",  "F",  "SH", "G",  "SH"],
+    ["B", "SH", "SH", "SH", "1",  "SH", "SH", "SH", "SH", "SH", "SH", "SH", "SH", "FC", "SH", "G",  "G",  "E",  "SP", "E",  "G",  "2K", "F",  "F",  "G", "2K", "2K", "2K", "SH", "SH", "SH"],
+]
+
+suicideSqueezeData = [
+    #     10    11    12    13    14    15    16    17    18    19    20    21    22    23   24   25   26   27    28    29    30    31    32   33    34    35    36    37   38   39    
+    ["A", "SH", "SH", "SH", "1",  "1",  "SH", "SH", "SH", "SH", "SH", "SH", "FC", "SH", "-", "-", "-", "-", "SH", "SH", "SH", "E",  "SH", "E", "F",  "2K", "2K", "SH", "G", "G", "-"],
+    ["B", "SH", "1",  "1",  "SH", "SH", "SH", "SH", "SH", "SH", "SH", "SH", "-",  "F",  "F", "-", "-", "-", "-",  "SH", "-",  "SH", "E",  "G", "2K", "2K", "2K", "-",  "-", "G", "FC"],
+]
+
+pitcherXData = [
+    #       10    11     12    13   14    15    16    17    18    19    20    21     22     23     24     25     26     27     28    29    30     31     32     33    34   35    36   37     38     39    
+    ["X",   "I",  "I",   "I",  "I", "I",  "I",  "I",  "I",  "I",  "I",  "WP", "WP",  "WP",  "WP",  "BK",  "WP",  "WP",  "BK",  "WP", "I",  "WP",  "WP",  "PB",  "PB", "H", "H", "PB", "WP",  "WP",  "WP"],
+    ["Pos", "1B", "1B",  "CF", "C", "SS", "SS", "SS", "1B", "1B", "1B", "CF", "CF",  "3B",  "P",   "2B",  "2B",  "3B",  "3B",  "2B", "1B", "RF",  "RF",  "SS",  "P",  "P", "C",  "C", "LF",  "LF",  "CF"],
+    ["Dur", "15", "N/I", "8",  "4", "3",  "2",  "7",  "10", "14", "20", "6",  "N/I", "N/I", "N/I", "N/I", "N/I", "N/I", "N/I", "5",  "12", "N/I", "N/I", "N/I", "0",  "0", "0",  "1", "N/I", "N/I", "9"],
+]
+
 def_inner_table_style = TableStyle([
     ('FONTNAME', (0, 0), (-1, -1), 'Helvetica-Bold'),  # set bold font
     ('FONTSIZE', (0,0), (-1,-1), 9),  # set font size for all cells to 9
@@ -43,57 +62,24 @@ for col in range(1, len(defData[1])):
     ddtable.setStyle(def_inner_table_style)
     defData[1][col] = ddtable
 
-# Prepare data for the table
-secondData = [
-    #     10   11   12   13   14   15   16   17   18   19    20   21   22   23   24   25   26   27   28      29     30   31     32   33     34   35   36   37   38   39    
-    ["0", "O", "O", "O", "P", "E", "O", "O", "O", "O", "BK", "O", "O", "O", "O", "O", "O", "O", "O", "S",    "S+E", "O", "O",   "O", "S",   "S", "S", "O", "S", "S", "S+E"],
-    ["1", "BK", "O", "S", "P", "E", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "S+E",  "O",   "O", "O",   "S", "S",   "S", "S", "S", "S", "S", "S+E"],
-    ["2", "O", "BK", "S", "P", "E", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "S", "S", "S", "S",    "S",   "S", "S+E", "S", "S",   "S", "S", "S", "S", "S", "S"],
-    ["3", "O", "O", "S", "P", "E", "O", "S", "O", "O", "O",  "O", "O", "O", "O", "O", "S", "S", "S", "S",    "S+E", "S", "S+E", "S", "S",   "S", "S", "S", "S", "S", "BK"],
-    ["4", "O", "BK", "S", "P", "E", "O", "O", "O", "O", "BK", "O", "O", "O", "O", "S", "S", "S", "S", "S+E", "S",   "S", "S+E", "S", "S",   "S", "S", "S", "S", "S", "S"],
-    ["5", "O", "O", "O", "P", "E", "S", "O", "S", "O", "S",  "O", "O", "O", "S", "S", "S", "S", "S", "BK",    "S",  "S", "S",   "S", "S+E", "S", "S", "S", "S", "S", "S"],
-]
-
-thirdData = [
-    #     10    11   12    13   14   15   16   17   18   19    20   21   22   23   24   25   26   27   28     29     30   31     32   33   34   35   36   37   38   39    
-    ["0", "O",  "E", "O",  "O", "O", "O", "O", "P", "O", "BK", "O", "O", "O", "O", "O", "O", "O", "O", "O",   "O",   "O", "O",   "O", "S", "S", "S", "O", "S", "S", "S+E"],
-    ["1", "O",  "E", "O",  "O", "O", "O", "O", "P", "O", "BK", "O", "O", "O", "O", "O", "O", "O", "O", "O",   "S+E", "O", "O",   "S", "S", "S", "S", "S", "S", "S", "S+E"],
-    ["2", "BK", "E", "O",  "O", "O", "O", "O", "P", "O", "S",  "O", "O", "O", "O", "O", "O", "O", "S", "S+E", "S",   "S", "S",   "S", "S", "S", "S", "S", "S", "S", "S+E"],
-    ["3", "BK", "E", "O",  "O", "O", "O", "O", "P", "O", "O",  "O", "O", "O", "O", "O", "S", "S", "S", "S",   "S",   "S", "S+E", "S", "S", "S", "S", "S", "S", "S", "S"],
-    ["4", "O",  "E", "O",  "S", "O", "O", "O", "P", "O", "O",  "O", "O", "O", "O", "S", "S", "S", "S", "S",   "S+E", "S", "S+E", "S", "S", "S", "S", "S", "S", "S", "BK"],
-    ["5", "O",  "E", "BK", "O", "O", "O", "O", "P", "O", "O",  "O", "O", "O", "S", "S", "S", "S", "S", "S+E", "S",   "S", "S+E", "S", "S", "S", "S", "S", "S", "S", "O"],
-]
-
-homeData = [
-    #     10   11    12    13     14   15   16   17   18    19   20   21   22   23   24   25   26   27   28     29     30   31   32   33   34   35   36   37   38     39    
-    ["0", "P", "O",  "O",  "S",   "O", "O", "O", "O", "BK", "E", "O", "O", "O", "O", "O", "O", "O", "O", "O",   "O",   "O", "O", "O", "O", "O", "O", "O", "O", "O",   "S+E"],
-    ["1", "P", "S",  "O",  "O",   "O", "S", "S", "S", "BK", "E", "O", "O", "O", "O", "O", "O", "O", "O", "O",   "O",   "O", "O", "O", "O", "O", "O", "O", "O", "O",   "S+E"],
-    ["2", "P", "O",  "O",  "O",   "S", "S", "S", "S", "BK", "O", "O", "O", "O", "O", "O", "O", "S", "S", "S+E", "E",   "O", "O", "O", "O", "O", "O", "O", "O", "O",   "O"],
-    ["3", "P", "BK", "S",  "O",   "O", "O", "O", "O", "E",  "O", "O", "O", "O", "O", "O", "O", "O", "O", "S",   "S+E", "S", "S", "O", "O", "O", "O", "S", "S", "S",   "S+E"],
-    ["4", "P", "BK", "S",  "O",   "O", "O", "O", "E", "S",  "O", "O", "O", "O", "O", "O", "O", "O", "O", "O",   "O",   "O", "O", "O", "O", "S", "S", "S", "S", "S+E", "O"],
-    ["5", "P", "E",  "BK", "S+E", "O", "O", "O", "O", "O",  "O", "O", "O", "O", "O", "O", "O", "O", "O", "O",   "S+E", "S", "S", "O", "O", "S", "S", "S", "S", "0",   "O"],
-]
-
-managerData = [
-    #     10    11    12   13    14    15    16    17   18    19   20   21   22    23     24    25     26     27     28     29    30   31   32    33    34    35    36    37    38   39    
-    ["0", "O",  "O",  "E", "O",  "O",  "O",  "O",  "O", "O",  "O", "O", "O", "O+", "O+",  "O+", "O+",  "O+",  "O",   "S+E", "O",  "O", "O", "O",  "O",  "S+", "S",  "S+", "S",  "O", "O"],
-    ["1", "O",  "O+", "O", "E",  "O",  "O",  "O",  "O", "O",  "O", "O", "O", "O",  "O+",  "O+", "O+",  "S",   "S+E", "O+",  "O",  "O", "O", "S+", "S+", "S",  "S",  "S",  "O",  "S", "O"],
-    ["2", "S",  "O+", "O", "O",  "E",  "O",  "O",  "O", "O",  "O", "O", "O", "O",  "O",   "O+", "O+",  "S+E", "S",   "S",   "S+", "S", "S", "S",  "S",  "S",  "S+", "S+", "S+", "S", "S"],
-    ["3", "O+", "E",  "O", "S",  "O",  "O+", "O+", "O", "O+", "O", "O", "O", "O",  "S+E", "O",  "S",   "S",   "S",   "S",   "S",  "S", "S", "S",  "S",  "S+", "S+", "S+", "S",  "S", "S"],
-    ["4", "O",  "E",  "O", "E",  "O+", "O+", "O",  "O", "O",  "S", "O", "O", "S",  "S",   "S",  "S+E", "S",   "S",   "S",   "S",  "S", "S", "S",  "S",  "S+", "S+", "S+", "S",  "S", "S"],
-    ["5", "O+", "E",  "S", "O+", "E",  "O",  "O",  "O", "O",  "S", "S", "S", "S",  "S",   "S",  "S",   "S+E", "S+E", "S",   "S",  "S", "S", "S",  "S",  "S+", "S+", "S+", "S+", "S", "S"],
-]
-
 keyData = [
-    ["S", "Stolen Base", "O", "Runner thrown out, other runners attemping to steal advance 1 base"],
-    ["S+E", "Stolen Base plus Error - All Runners advance another base on the error", "P", "Runner picked off, other runners hold"],
-    ["E", "Runner safe on Error - Other runners attemping to steal advance 1 base", "BK", "Balk - All runners advance one base"],
+    ["1", "Batter Safe and all runners advance 1 base", "F", "Pop out - on squeeze DP"],
+    ["SH", "Sacrifice - batter out, all runners advance 1 base", "G", "Routine ground out force"],
+    ["FC", "Sacrifice successfull and fielder's choice - batter safe at first", "DP", "Standard DP"],
+    ["E", "Sacrifice successfull plus batter safe at first on error", "-", "Missed bunt - runner must attempt to steal home"],
+    ["2K", "Two foul bunts - batter may attempt again, but if another 2K batter strikes out and batter must attempt", "", ""],
+    ["", "to steal.  If batter chooses not to bunt again, pitcher gets SO on 35 and 36 and rolls to pitch.", "", ""],
 ]
 
 key2Data = [
-    ["S", "Runner Safe - All runners not trying to advance hold", "E", "Runner safe on Error - All runners not trying to advance hold"],
-    ["S+E", "Stolen Safe PLUS error - All runners advance an extra base on the error", "O+", "Runner out - All runners behind the runner advance 1 base"],
-    ["S+", "Runner Safe - All runners behind the runner advance 1 base", "O", "Runner out - All runners not trying to advance hold"],
+    ["BK", "Balk - all runners advance one base"],
+    ["H", "Batter hit by pitch and is awarded first base (roll again on 'Dur' table to determine injury effect to batter)"],
+    ["PB", "Passed Ball - all runners advance one base"],
+    ["WP", "Wild Pitch - all runners advance one base"],
+    ["I", "Possible Injury to player in the field, roll again on the 'Pos' chart to determine which player may be injured, the roll on the 'Dur' to determine duration."],
+    ["#", "When a number appears for an injury duration, the player is removed from the game and must sit out a number of games equal to that shown."],
+    ["0", "When a zero appears for an injury duration, the player is out for the remainder of the game."],
+    ["N/I", "When N/I appears there is no injury."],
 ]
 
 # Create a PDF with 'names_ages.pdf' as the name
@@ -128,16 +114,15 @@ for i in range(10, 40):
     header.append(Paragraph(str(i), header_style))
     colwidths.append(23)
 
-secondData.insert(0, header)
-thirdData.insert(0, header)
-homeData.insert(0, header)
-managerData.insert(0, header)
+sacBuntData.insert(0, header)
+suicideSqueezeData.insert(0, header)
+pitcherXData.insert(0, header)
 
 # Create a Table object
 defTable = Table(defData, colWidths=[30,30,30,30], rowHeights=[40,60])
-thirdTable = Table(thirdData, colWidths=colwidths, rowHeights=[18,12,12,12,12,12,12])
-homeTable = Table(homeData, colWidths=colwidths, rowHeights=[18,12,12,12,12,12,12])
-managerTable = Table(managerData, colWidths=colwidths, rowHeights=[18,12,12,12,12,12,12])
+sacBuntTable = Table(sacBuntData, colWidths=colwidths, rowHeights=[18,12,12])
+suicideSqueezeTable = Table(suicideSqueezeData, colWidths=colwidths, rowHeights=[18,12,12])
+pitcherXTable = Table(pitcherXData, colWidths=colwidths, rowHeights=[18,12,12,12])
 
 def_table_style = TableStyle([
     ('GRID', (0,0), (-1,-1), 1, colors.black),
@@ -163,9 +148,9 @@ table_style = TableStyle([
     ('ALIGN', (0,0), (-1,-1), 'CENTER'),  # center text in all cells
 ])
 defTable.setStyle(def_table_style)
-thirdTable.setStyle(table_style)
-homeTable.setStyle(table_style)
-managerTable.setStyle(table_style)
+sacBuntTable.setStyle(table_style)
+suicideSqueezeTable.setStyle(table_style)
+pitcherXTable.setStyle(table_style)
 
 # Create a title with a style and center alignment
 title_style = ParagraphStyle(
@@ -175,61 +160,81 @@ title_style = ParagraphStyle(
     alignment=1  # Center alignment
 )
 defTitle = Paragraph('Automatic Out', title_style)
-thirdTitle = Paragraph('Steal of Third Base', title_style)
-homeTitle = Paragraph('Steal of Home', title_style)
-managerTitle = Paragraph("Manager's Decision Chart (referred to by results including parenthesis)", title_style)
+sacBuntTitle = Paragraph('Sacrifice Bunt (to advance runners to 2nd or 3rd base)', title_style)
+suicideSqueezeTitle = Paragraph('Suicide Squeeze Bunt (to advance a runner home)', title_style)
+pitcherXTitle = Paragraph('Pitcher X Chart', title_style)
 
+sub_title_style = ParagraphStyle(
+    'SubTitle', 
+    parent=getSampleStyleSheet()['Heading2'], 
+    fontSize=9,
+    alignment=1  # Center alignment
+)
+pitcherXSubTitle = Paragraph('When a pitcher rolls a 38 roll again on the "X" chart above.  Simply roll and crossreference to determine the outcome.', sub_title_style)
 
 story = []
 
-# Steal of Second
+# Defense Chart
 story.append(defTitle)  # Add the title to the story
 story.append(Spacer(1, -10))  # Add some space below the title
 story.append(defTable)
 
-#story.append(Spacer(1, 12))
+story.append(Spacer(1, 12))
 
-# Steal of Third
-#story.append(thirdTitle)  # Add the title to the story
-#story.append(Spacer(1, -10))  # Add some space below the title
-#story.append(thirdTable)
+# Sac Bunt
+story.append(sacBuntTitle)  # Add the title to the story
+story.append(Spacer(1, -10))  # Add some space below the title
+story.append(sacBuntTable)
 
-#story.append(Spacer(1, 12))
+story.append(Spacer(1, 12))
 
-# Steal of Home
-#story.append(homeTitle)  # Add the title to the story
-#story.append(Spacer(1, -10))  # Add some space below the title
-#story.append(homeTable)
+# Suicide Squeeze
+story.append(suicideSqueezeTitle)  # Add the title to the story
+story.append(Spacer(1, -10))  # Add some space below the title
+story.append(suicideSqueezeTable)
 
 # Key Chart
 story.append(Spacer(1, 5))  # Add some space below the title
-keyTable = Table(keyData, colWidths=[24,320,24,320], rowHeights=[12,12,12])
+keyTable = Table(keyData, colWidths=[24,320,24,320], rowHeights=[12,12,12,12,12,12])
 key_table_style = TableStyle([
-    ('GRID', (0,0), (0,2), 1, colors.black),
-    ('GRID', (2,0), (2,2), 1, colors.black),
+    ('GRID', (0,0), (0,4), 1, colors.black),
+    ('GRID', (2,0), (2,3), 1, colors.black),
     ('INNERPADDING', (0,0), (-1,-1), 1),
     ('OUTERPADDING', (0,0), (-1,-1), 1),
     ('FONTNAME', (0, 0), (-1, -1), 'Helvetica-Bold'),  # set bold font
     ('FONTSIZE', (0,0), (-1,-1), 9),  # set font size for all cells
     ('TEXTCOLOR', (0,0), (-1,-1), colors.black),  # set text color for all cells to black
     ('LEADING', (0, 0), (-1, -1), 9),  # set leading value
-    ('ALIGN', (0,0), (0,2), 'CENTER'),
-    ('ALIGN', (2,0), (2,2), 'CENTER'),
+    ('ALIGN', (0,0), (0,5), 'CENTER'),
+    ('ALIGN', (2,0), (2,4), 'CENTER'),
 ])
 keyTable.setStyle(key_table_style)
-#story.append(keyTable)
+story.append(keyTable)
 
-#story.append(Spacer(1, 12))
+story.append(Spacer(1, 12))
 
-#story.append(managerTitle)  # Add the title to the story
-#story.append(Spacer(1, -10))  # Add some space below the title
-#story.append(managerTable)
+story.append(pitcherXTitle)
+story.append(Spacer(1, -10))
+story.append(pitcherXTable)
+story.append(Spacer(1, -8))
+story.append(pitcherXSubTitle)
+story.append(Spacer(1, -8))
 
 # Key 2 Chart
-#story.append(Spacer(1, 5))  # Add some space below the title
-key2Table = Table(key2Data, colWidths=[24,320,24,320], rowHeights=[12,12,12])
-key2Table.setStyle(key_table_style)
-#story.append(key2Table)
+story.append(Spacer(1, 5))  # Add some space below the title
+key2Table = Table(key2Data, colWidths=[24,600], rowHeights=[12,12,12,12,12,12,12,12])
+key2_table_style = TableStyle([
+    ('GRID', (0,0), (0,7), 1, colors.black),
+    ('INNERPADDING', (0,0), (-1,-1), 1),
+    ('OUTERPADDING', (0,0), (-1,-1), 1),
+    ('FONTNAME', (0, 0), (-1, -1), 'Helvetica-Bold'),  # set bold font
+    ('FONTSIZE', (0,0), (-1,-1), 9),  # set font size for all cells
+    ('TEXTCOLOR', (0,0), (-1,-1), colors.black),  # set text color for all cells to black
+    ('LEADING', (0, 0), (-1, -1), 9),  # set leading value
+    ('ALIGN', (0,0), (0,7), 'CENTER'),
+])
+key2Table.setStyle(key2_table_style)
+story.append(key2Table)
 
 # Build the PDF
 pdf.build(story)
